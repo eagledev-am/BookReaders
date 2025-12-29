27635e213d40ef367d7b3905c439d235696241e1
Abdelrahman Magdy <magdyabdo484@gmail.com>
2025-12-29 23:29:03 +0200

    feat: verification code entity to verify user

---
+++ src/main/java/com/eagledev/bookreaders/entities/VerificationCode.java
@@ -0,0 +1,42 @@
+package com.eagledev.bookreaders.entities;
+
+import com.eagledev.bookreaders.entities.enums.CodeType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "verification_code")
+public class VerificationCode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String code;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CodeType type; // Enum defined below
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public boolean isExpired() {
+        return LocalDateTime.now().isAfter(expiresAt);
+    }
+}
