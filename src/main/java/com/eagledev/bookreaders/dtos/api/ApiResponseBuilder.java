dd70c8727ec9550532d579afe53f7543f0010f95
Abdelrahman Magdy <magdyabdo484@gmail.com>
2026-01-02 03:17:51 +0200

    feat: Add a unifed api response way

---
+++ src/main/java/com/eagledev/bookreaders/dtos/api/ApiResponseBuilder.java
@@ -0,0 +1,19 @@
+package com.eagledev.bookreaders.dtos.api;
+
+public class ApiResponseBuilder {
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, message, data);
+    }
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, "Success", data);
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(false, message, null);
+    }
+
+    public static <T> ApiResponse<T> error(String message, T data) {
+        return new ApiResponse<>(false, message, data);
+    }
+}
