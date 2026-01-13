2433d90139ddb2a3cbde3703cdb0e2e3ccc2dd36
Abdelrahman Magdy <magdyabdo484@gmail.com>
2026-01-13 17:06:23 +0200

    chore: Add handler for AuthorizationDeniedException

--- src/main/java/com/eagledev/bookreaders/exceptions/HandlingException.java
+++ src/main/java/com/eagledev/bookreaders/exceptions/HandlingException.java
@@ -9,6 +9,7 @@ import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -54,6 +55,13 @@ public class HandlingException {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ApiResponseBuilder.error(ex.getMessage()));
     }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<ApiResponse<Object>> authorizationDeniedException(AuthorizationDeniedException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(ApiResponseBuilder.error(ex.getMessage()));
+    }
+
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ApiResponse<Object>> handleBadRequest(BadRequestException ex) {
