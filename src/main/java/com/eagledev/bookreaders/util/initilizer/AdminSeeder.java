20231ac753f9624bb65631c9d39b7f8b6832f2c9
Abdelrahman Magdy <magdyabdo484@gmail.com>
2026-03-04 01:33:15 +0200

    chore: Add an admin seeder to add an admin account

--- src/main/java/com/eagledev/bookreaders/util/initilizer/AdminSeeder.java
+++ src/main/java/com/eagledev/bookreaders/util/initilizer/AdminSeeder.java
@@ -5,6 +5,7 @@ import com.eagledev.bookreaders.entities.enums.Role;
 import com.eagledev.bookreaders.repos.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,18 +17,24 @@ public class AdminSeeder implements CommandLineRunner {
     private final UserRepo userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Value("${app.admin.email}")
+    private String email;
+
+    @Value("${app.admin.password}")
+    private String password;
+
     @Override
     public void run(String... args) throws Exception {
 
-        if (userRepository.existsByEmail("admin@eagledev.com")) {
+        if (userRepository.existsByEmail(email)) {
             log.info("--> Admin user already exists.");
             return;
         }
 
         User admin = User.builder()
                 .name("Super")
-                .email("admin@eagledev.com")
-                .password(passwordEncoder.encode("admin"))
+                .email(email)
+                .password(passwordEncoder.encode(password))
                 .role(Role.ADMIN)
                 .enabled(true)
                 .build();
